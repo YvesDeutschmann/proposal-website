1100cade8dad97802e5c159514ac908161a08c69
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-10-03 18:48:19 +0200

    feat: added card_deck page

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
 from app import app
 from templates import home, prediction, card_deck
@@ -32,8 +32,31 @@ def display_page(pathname):
         return home.layout
     elif pathname == "/prediction":
         return prediction.layout
+    elif pathname == "/cards":
+        return card_deck.layout
     else:
         return '404'
+
+# Card Deck callbacks
+@app.callback(
+    Output("yes_collapse", "is_open"),
+    [Input("answer_button", "n_clicks")],
+    [State("yes_button", "is_open")],
+)
+def toggle_yes(click, is_open):
+    if click:
+        return not is_open
+    return is_open
+
+@app.callback(
+    Output("no_collapse", "is_open"),
+    [Input("answer_button", "n_clicks")],
+    [State("no_button", "is_open")],
+)
+def toggle_left(click, is_open):
+    if click:
+        return not is_open
+    return is_open
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- templates/card_deck.py
+++ templates/card_deck.py
@@ -4,14 +4,135 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
 
-layout = html.Div([
-    html.H2('Page 1'),
-    dcc.Input(id='input-1-state', type='text', value='Montreal'),
-    dcc.Input(id='input-2-state', type='text', value='Canada'),
-    html.Button(id='submit-button', n_clicks=0, children='Submit'),
-    html.Div(id='output-state'),
-    html.Br(),
-    dcc.Link('Navigate to "/"', href='/'),
-    html.Br(),
-    dcc.Link('Navigate to "/page-2"', href='/page-2'),
-])
+from app import app
+
+empty_card = dbc.Card()
+
+card = dbc.Card(
+    [
+        dbc.CardImg(src='/static/sample200.png'),
+        dbc.CardHeader(html.H4("Someones Name")),
+        dbc.CardBody(html.P("Some funny punchline")),
+    ]
+)
+
+my_card = dbc.Card(
+    [
+        dbc.CardImg(src='/static/sample200.png'),
+        dbc.CardHeader(html.H4("Mr. Bossypants")),
+        dbc.Button(
+            "Answer", 
+            id='answer_button',
+            color='primary',
+            block=True), 
+    ]
+)
+
+yes_card = dbc.Collapse(
+    dbc.Card(
+        [
+            dbc.CardHeader(),
+            dbc.CardImg(src='https://media.giphy.com/media/Ld0P4AdeSDjoY/source.gif'),
+            dbc.Button("Yes", id='yes_button', color='success'),
+            dbc.CardFooter()
+        ]
+    ),
+    id='yes_collapse'
+)
+
+no_card = dbc.Collapse(
+    dbc.Card(
+        [
+            dbc.CardHeader(),
+            dbc.CardImg(src='https://media.giphy.com/media/1zSz5MVw4zKg0/source.gif'),
+            dbc.Button("No", id='no_button', color='danger'),
+            dbc.CardFooter()
+        ]
+    ),
+    id='no_collapse'
+)
+
+row1 = dbc.CardGroup(
+    [
+        card, 
+        card,
+        card, 
+        card, 
+        empty_card,
+        card,
+        card,
+        card
+    ]
+)
+
+row2 = dbc.CardGroup(
+    [
+        card, 
+        card,
+        card,
+        card,
+        card,
+        empty_card,
+        card,
+        card
+    ]
+)
+
+row3 = dbc.Row(
+    [
+        dbc.Col(empty_card),
+        dbc.Col(yes_card),
+        dbc.Col(empty_card),
+        dbc.Col(my_card),
+        dbc.Col(empty_card),
+        dbc.Col(no_card),
+        dbc.Col(empty_card),
+    ],
+    
+)
+
+final_decision = dbc.Row(
+    [
+        dbc.Col(
+            dbc.Button(
+            "Yes", 
+            id='yes_button',
+            color='success',
+            block=True), 
+        width=4),
+        dbc.Col(
+            dbc.Button(
+            "No", 
+            id='no_button',
+            color='danger',
+            block=True), 
+        width=4),
+    ],
+    justify='center'
+)
+
+layout = html.Div(
+    [
+        dbc.Row(
+            dbc.Col(row1),
+            style={"width":"100%"}
+        ),
+        dbc.Row(
+            dbc.Col(row2),
+            style={"width":"100%"}
+        ),
+        row3
+        # dbc.Row(
+        #     [
+        #         dbc.Col(
+        #             dbc.Button(
+        #             "Yes", 
+        #             id='yes_button',
+        #             color='success',
+        #             block=True), 
+        #         width=4),
+        #     ]
+        #     my_card, justify='center'),
+        # final_decision
+    ]
+)
