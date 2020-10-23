96bd0299b684050096ccb3fd7a43a066be944581
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-10-23 13:31:13 +0200

    feat: added flip rotation on test container

--- templates/card_deck.py
+++ templates/card_deck.py
@@ -2,7 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output, State, ClientsideFunction
 
 from app import app
 
@@ -10,22 +10,69 @@ empty_card = dbc.Card()
 
 card = dbc.Card(
     [
-        dbc.CardImg(src='/static/sample200.png'),
-        dbc.CardHeader(html.H4("Someones Name")),
-        dbc.CardBody(html.P("Some funny punchline")),
-    ]
+        
+                dbc.CardImg(src='/static/question.png', className="card-img"),
+                dbc.CardImg(src='/static/sample200.png'),
+                dbc.CardHeader(html.H4("Someones Name")),
+                dbc.CardBody(html.P("Some funny punchline"))
+    ],
+    className="card"
 )
 
-my_card = dbc.Card(
+# card = html.Div(
+#     [
+#         html.Div(
+#             [
+#                 html.Img(src='/static/question.png', className="card-img"),
+#                 html.Div(
+#                     [
+#                         html.Img()
+#                     ],
+#                     className="card-img-overlay"
+#                 )
+#             ],
+#             className="card-inner"
+#         )
+        
+#                 dbc.CardImg(src='/static/question.png', className="card-img"),
+#                 dbc.CardImg(src='/static/sample200.png'),
+#                 dbc.CardHeader(html.H4("Someones Name")),
+#                 dbc.CardBody(html.P("Some funny punchline"))
+#     ],
+#     className="card bg-dark text-white"
+# )
+
+front = dbc.Card(
+                html.Img(src='/static/question.png'),
+                id = "front",
+                className="front"
+)
+
+back = dbc.Card(
     [
-        dbc.CardImg(src='/static/sample200.png'),
-        dbc.CardHeader(html.H4("Mr. Bossypants")),
-        dbc.Button(
+        html.Img(src='/static/sample200.png'),
+        html.H4("Mr. Bossypants"),
+        html.Button(
             "Answer", 
             id='answer_button',
-            color='primary',
-            block=True), 
-    ]
+            # color='primary',
+            # block=True
+        ),
+    ],
+    id = "back",
+    className="back"
+)
+
+my_card = html.Div(
+    html.Div(
+        [
+            front, 
+            back
+        ],
+        id = "card",
+        className = "card",        
+    ),
+    className="container"
 )
 
 yes_card = dbc.Collapse(
@@ -111,28 +158,57 @@ final_decision = dbc.Row(
     justify='center'
 )
 
+test = html.Div(
+    [
+        html.Div(
+            [
+                html.Img(
+                    src='/static/question.png',
+                    className = "cards_image"
+                ),
+                html.H1("Click Me!")
+            ],
+            className = "cards_front"
+        ),
+        html.Div(
+            [
+                html.Img(src='/static/sample200.png'),
+                html.H1("Some Name"),
+                html.P("Some funnyx text...")
+            ],
+            className = "cards_back"
+        )
+    ],
+    className = "cards_single",
+    id = 'test'
+)
+
 layout = html.Div(
     [
-        dbc.Row(
-            dbc.Col(row1),
-            style={"width":"100%"}
-        ),
-        dbc.Row(
-            dbc.Col(row2),
-            style={"width":"100%"}
-        ),
-        row3
         # dbc.Row(
-        #     [
-        #         dbc.Col(
-        #             dbc.Button(
-        #             "Yes", 
-        #             id='yes_button',
-        #             color='success',
-        #             block=True), 
-        #         width=4),
-        #     ]
-        #     my_card, justify='center'),
-        # final_decision
+        #     dbc.Col(row1),
+        #     style={"width":"100%"}
+        # ),
+        # dbc.Row(
+        #     dbc.Col(row2),
+        #     style={"width":"100%"}
+        # ),
+        # row3
+        dbc.Col(
+            test,
+            width=2
+        )
+        
     ]
 )
+
+@app.callback(
+    Output('test', 'className'),
+    [Input('test', 'n_clicks')]
+)
+def flip_card(n_clicks):
+    
+    if (n_clicks % 2 == 0):
+        return "cards_single"
+    else:
+        return "cards_single flip"
