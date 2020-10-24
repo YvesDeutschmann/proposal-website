e7d9ba0b6df59b72e6f386db64c18d019aa8e39e
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-10-24 08:25:19 +0200

    chore: added static content

---
+++ static/pics/assignments.py
@@ -0,0 +1,86 @@
+assignments = {
+    11: {
+        'id': 11,
+        'name': 'Bonnie & Jerry',
+        'image': '11.jpg',
+        'card':
+    },
+    12: {
+        'id': 12,
+        'name': 'Rachel',
+        'image': '12.jpg',
+        'card':
+    },
+    13: {
+        'id': 13,
+        'name': 'Oma & Hans',
+        'image': '13.jpg',
+        'card':
+    },
+    14: {
+        'id': 14,
+        'name': 'Keenan & Sofian',
+        'image': '14.jpg',
+        'card':
+    },
+    15: {
+        'id': 15,
+        'name': 'Mariana',
+        'image': '15.jpg',
+        'card':
+    },
+    16: {
+        'id': 16,
+        'name': 'Grandma & Bryan',
+        'image': '16.jpg',
+        'card':
+    },
+    17: {
+        'id': 17,
+        'name': 'Marley',
+        'image': '17.jpg',
+        'card':
+    },
+    21: {
+        'id': 21,
+        'name': 'Emily',
+        'image': '21.jpg',
+        'card':
+    },
+    22: {
+        'id': 22,
+        'name': 'Tiffany',
+        'image': '22.jpg',
+        'card':
+    },
+    23: {
+        'id': 23,
+        'name': 'Gris & Andre',
+        'image': '23.jpg',
+        'card':
+    },
+    24: {
+        'id': 24,
+        'name': 'Taylor & Lindsay',
+        'image': '25.jpg',
+        'card':
+    },
+    25: {
+        'id': 25,
+        'name': 'Flavia & Dennis',
+        'image': '25.jpg',
+        'card':
+    },
+    26: {
+        'id': 26,
+        'name': 'Alex & Jenny',
+        'image': '26.jpg',
+        'card':
+    },
+    27: {
+        'id': 27,
+        'name': 'Malayna & Greg',
+        'image': '27.jpg',
+        'card':
+    }
+}
