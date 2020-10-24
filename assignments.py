b2a0f211cb99f433dd6a80b09aa4a9e4a26b26e5
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-10-24 15:34:54 +0200

    feat: added assignments dict as backlground data

---
+++ assignments.py
@@ -0,0 +1,77 @@
+assignments = {
+    '11': {
+        'id': '11',
+        'name': 'Bonnie & Jerry',
+        'image': '/static/11.jpg',
+    },
+    '12': {
+        'id': '12',
+        'name': 'Rachel',
+        'image': '12.jpg',
+    },
+    '13': {
+        'id': '13',
+        'name': 'Oma & Hans',
+        'image': '13.jpg',
+    },
+    '14': {
+        'id': '14',
+        'name': 'Keenan & Sofian',
+        'image': '14.jpg',
+    },
+    '15': {
+        'id': '15',
+        'name': 'Mariana',
+        'image': '15.jpg',
+    },
+    '16': {
+        'id': '16',
+        'name': 'Grandma & Bryan',
+        'image': '16.jpg',
+    },
+    '17': {
+        'id': '17',
+        'name': 'Marley',
+        'image': '17.jpg',
+    },
+    '21': {
+        'id': '21',
+        'name': 'Emily',
+        'image': '21.jpg',
+    },
+    '22': {
+        'id': '22',
+        'name': 'Tiffany',
+        'image': '22.jpg',
+    },
+    '23': {
+        'id': '23',
+        'name': 'Gris & Andre',
+        'image': '23.jpg',
+    },
+    '24': {
+        'id': '24',
+        'name': 'Taylor & Lindsay',
+        'image': '24.jpg',
+    },
+    '25': {
+        'id': '25',
+        'name': 'Flavia & Dennis',
+        'image': '25.jpg',
+    },
+    '26': {
+        'id': '26',
+        'name': 'Alex & Jenny',
+        'image': '26.jpg',
+    },
+    '27': {
+        'id': '27',
+        'name': 'Malayna & Greg',
+        'image': '27.jpg',
+    },
+    '31': {
+        'id': '31',
+        'name': 'Mr. Bossypants',
+        'image': '31.jpg',
+    }
+}
