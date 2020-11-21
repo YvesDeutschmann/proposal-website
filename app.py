50ac1fdf4c5ffc6519e5c3821b24cccb594f5ade
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-11-21 18:52:14 +0100

    deployment: update Procfile, add server variable

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ import dash_bootstrap_components as dbc
 # create app object
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-server = app.server
+
