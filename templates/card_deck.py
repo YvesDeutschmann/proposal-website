33e93a8b4e5e169be185e404f271bfbcd72357b0
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-12-04 13:16:21 +0100

    feat: get images from aws s3 storage

--- templates/card_deck.py
+++ templates/card_deck.py
@@ -23,7 +23,7 @@ def generate_card(id, image_src, name):
         [
             html.Div(
                 html.Img(
-                        src=app.get_asset_url('question.png'),
+                        src='https://proposal-pics.s3.eu-central-1.amazonaws.com/question.png',
                         className = "cards_image"
                 ),
                 className = "cards_front"
@@ -31,7 +31,7 @@ def generate_card(id, image_src, name):
             html.Div(
                 [
                     html.Img(
-                        src=app.get_asset_url(image_src),
+                        src=image_src,
                         className = "cards_image"
                     ),
                     html.H1(
@@ -60,7 +60,7 @@ def generate_mycard(id, image_src, name):
         [
             html.Div(
                 html.Img(
-                        src=app.get_asset_url('question.png'),
+                        src='https://proposal-pics.s3.eu-central-1.amazonaws.com/question.png',
                         className = "cards_image"
                 ),
                 className = "cards_front"
@@ -68,7 +68,7 @@ def generate_mycard(id, image_src, name):
             html.Div(
                 [
                     html.Img(
-                        src=app.get_asset_url(image_src),
+                        src=image_src,
                         className='cards_image'
                     ),
                         
@@ -199,9 +199,9 @@ def create_card_deck():
 
     for key, element in assignments.items():
         if key == '31':
-            card = generate_mycard(element.get("id"), element.get("image"), element.get("name"))
+            card = generate_mycard(element.get("id"), element.get("source"), element.get("name"))
         else:
-            card = generate_card(element.get("id"), element.get("image"), element.get("name"))
+            card = generate_card(element.get("id"), element.get("source"), element.get("name"))
         
         card_deck.append(element.get("id"))
         assignments.update({ str(key) : {'card': card}})
