059b2d18768fd0a15a373e94b18002f828c145dc
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-12-04 15:29:33 +0100

    chore: changed S3 server to US

--- assignments.py
+++ assignments.py
@@ -3,90 +3,90 @@ assignments = {
         'id': '11',
         'name': 'Bonnie & Jerry',
         'image': '11.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/11.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/11.jpg'
     },
     '12': {
         'id': '12',
         'name': 'Rachel',
         'image': '12.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/12.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/12.jpg'
     },
     '13': {
         'id': '13',
         'name': 'Oma & Hans',
         'image': '13.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/13.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/13.jpg'
     },
     '14': {
         'id': '14',
         'name': 'Keenan & Sofian',
         'image': '14.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/14.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/14.jpg'
     },
     '15': {
         'id': '15',
         'name': 'Mariana',
         'image': '15.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/15.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/15.jpg'
     },
     '16': {
         'id': '16',
         'name': 'Eleanor & Bryan',
         'image': '16.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/16.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/16.jpg'
     },
     '17': {
         'id': '17',
         'name': 'Emily',
         'image': '17_copy.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/17_copy.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/17_copy.jpg'
     },
     '21': {
         'id': '21',
         'name': 'Marley',
         'image': '21.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/21.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/21.jpg'
     },
     '22': {
         'id': '22',
         'name': 'Tiffany',
         'image': '22.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/22.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/22.jpg'
     },
     '23': {
         'id': '23',
         'name': 'Gris & Andre',
         'image': '23.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/23.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/23.jpg'
     },
     '24': {
         'id': '24',
         'name': 'Taylor & Lindsay',
         'image': '24.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/24.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/24.jpg'
     },
     '25': {
         'id': '25',
         'name': 'Flavia & Dennis',
         'image': '25.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/25.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/25.jpg'
     },
     '26': {
         'id': '26',
         'name': 'Alex & Jenny',
         'image': '26.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/26.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/26.jpg'
     },
     '27': {
         'id': '27',
         'name': 'Malayna & Greg',
         'image': '27.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/27.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/27.jpg'
     },
     '31': {
         'id': '31',
         'name': 'Mr. Bossypants',
         'image': '31.jpg',
-        'source': 'https://proposal-pics.s3.eu-central-1.amazonaws.com/31.jpg'
+        'source': 'https://proposal-pics.s3-us-west-2.amazonaws.com/31.jpg'
     }
 }
