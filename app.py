ba13194b76d736ad4ffe720962abc10b94fdc9b2
YvesDeutschmann <yves.deutschmann@gmail.com>
2020-12-09 19:37:55 +0100

    feat: added information regarding flask server

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import flask
 import dash
 import dash_bootstrap_components as dbc
 
 # create app object
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css', dbc.themes.BOOTSTRAP]
+server = flask.Flask(__name__)
 app = dash.Dash(
-    __name__, 
+    __name__,
+    server=server,
     external_stylesheets=external_stylesheets,
     assets_external_path='https://proposal-pics.s3-us-west-2.amazonaws.com/')
 app.scripts.config.serve_locally=False
